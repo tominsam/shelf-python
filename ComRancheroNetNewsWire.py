4e402562d40c513a3f197662f344f1882bc29e90
tomi <tomi@ab05c693-d3d1-0310-8e8b-e1346d90d6a5>
2008-01-10 14:23:51 +0000

    use headline target url first.
    git-svn-id: https://jerakeen.org/svn/tomi/Projects/Shelf@3718 ab05c693-d3d1-0310-8e8b-e1346d90d6a5

--- ComRancheroNetNewsWire.py
+++ ComRancheroNetNewsWire.py
@@ -9,5 +9,4 @@ class ComRancheroNetNewsWire(Extractor):
 
     def clues(self):
         if self.nnw.selectedHeadline().exists() == 0: return [] 
-        url = self.nnw.selectedHeadline().subscription().homeURL()
-        return self.clues_from_url( url )
+        return self.clues_from_url( self.nnw.selectedHeadline().URL() ) + self.clues_from_url( self.nnw.selectedHeadline().subscription().homeURL() )
