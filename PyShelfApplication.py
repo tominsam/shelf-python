05c16c5cf42cce0987dd6d066df7d7fd62d2da10
Anil Madhavapeddy <anil@recoil.org>
2010-04-16 10:22:31 +0100

    disable hotkeys until I figure out whats broken in Snow Leopard

--- PyShelfApplication.py
+++ PyShelfApplication.py
@@ -3,7 +3,7 @@ from AppKit import *
 
 # based on example at http://svn.red-bean.com/pyobjc/trunk/pyobjc/pyobjc-framework-Cocoa/Examples/AppKit/HotKeyPython/
 
-from Carbon.CarbonEvt import RegisterEventHotKey, GetApplicationEventTarget
+#from Carbon.CarbonEvt import RegisterEventHotKey, GetApplicationEventTarget
 from Carbon.Events import cmdKey, controlKey
 
 kEventHotKeyPressedSubtype = 6
@@ -14,8 +14,8 @@ class PyShelfApplication(NSApplication):
     def finishLaunching(self):
         super(PyShelfApplication, self).finishLaunching()
         # register cmd-control-J
-        if NSUserDefaults.standardUserDefaults().boolForKey_("useHotkey"):
-            self.hotKeyRef = RegisterEventHotKey(38, cmdKey | controlKey, (0, 0), GetApplicationEventTarget(), 0)
+        #if NSUserDefaults.standardUserDefaults().boolForKey_("useHotkey"):
+        #    self.hotKeyRef = RegisterEventHotKey(38, cmdKey | controlKey, (0, 0), GetApplicationEventTarget(), 0)
 
     def sendEvent_(self, theEvent):
         if theEvent.type() == NSSystemDefined and theEvent.subtype() == kEventHotKeyPressedSubtype:
